8b2d972ad2684a45ddbfcf66a8711d56b3fd1cac
ollisam <olivers23@ru.is>
2025-10-15 15:44:21 +0000

    Update collision_avoidance.py

--- LAB8/collision_avoidance.py
+++ LAB8/collision_avoidance.py
@@ -63,7 +63,8 @@ def main():
             # If already moving forward and we get too close: hard stop
             if moving_forward and too_close(last_voltage):
                 direction = ord('s')
-                time.sleep(0.1)
+                time.sleep(0.001)
+                direction = -1
                 robot.stop()
                 moving_forward = False
                 stdscr.addstr(1, 10, f"STOP: obstacle! V={last_voltage:0.2f}V      ")
