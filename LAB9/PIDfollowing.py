bd9495721fb9ff1f409c7bddee51fee287645382
ollisam <olivers23@ru.is>
2025-10-27 15:07:24 +0000

    Update PIDfollowing.py

--- LAB9/PIDfollowing.py
+++ LAB9/PIDfollowing.py
@@ -20,7 +20,7 @@ tcs.gain = 4                # valid gains: 1, 4, 16, 60
 # -----------------------
 Kp = 24
 Ki = 0.01
-Kd = 200
+Kd = 250
 
 # The original code used "PWM" in [0..255]. We'll compute in that domain,
 # then map to Robot's [-1..1].
