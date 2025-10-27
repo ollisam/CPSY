47a7fa5b8b9df39b11dfc545afc3acbc215310a4
ollisam <olivers23@ru.is>
2025-10-27 13:19:42 +0000

    Update PIDfollowing.py

--- LAB9/PIDfollowing.py
+++ LAB9/PIDfollowing.py
@@ -19,7 +19,7 @@ tcs.gain = 4                # valid gains: 1, 4, 16, 60
 # PID + control params
 # -----------------------
 Kp = 24
-Ki = 1
+Ki = 2
 Kd = 1
 
 # --- Global speed limits (for gpiozero outputs) ---
