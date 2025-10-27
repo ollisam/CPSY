ffc39c1b9e26d6dd7f9bbdacdac22dd807783294
ollisam <olivers23@ru.is>
2025-10-27 13:46:49 +0000

    shut up

--- LAB9/PID.py
+++ LAB9/PID.py
@@ -18,9 +18,9 @@ tcs.gain = 4                # valid gains: 1, 4, 16, 60
 # -----------------------
 # PID + control params
 # -----------------------
-Kp = 0.5
+Kp = 0.7
 Ki = 0.0
-Kd = 0.0
+Kd = 0.25
 
 # The original code used "PWM" in [0..255]. We'll compute in that domain,
 # then map to Robot's [-1..1].
