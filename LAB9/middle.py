4ebbdece67c5d13633045580a6d997420d71a813
ollisam <olivers23@ru.is>
2025-10-27 14:47:57 +0000

    Update middle.py

--- LAB9/middle.py
+++ LAB9/middle.py
@@ -63,7 +63,7 @@ def clamp(x, lo, hi):
 
 def pwm_to_robot_speed(pwm_value):
     pwm_value = clamp(pwm_value, 0, 255)
-    pwm_value = (pwm_value / 255.0) * 0.13
+    pwm_value = (pwm_value / 255.0) * 0.2
     return pwm_value
 
 def set_motors(left_pwm_signed, right_pwm_signed):
