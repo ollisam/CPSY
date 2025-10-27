a1b69a37cfd36057242f990b55a15df417f762d5
ollisam <olivers23@ru.is>
2025-10-27 15:39:31 +0000

    Update PIDfollowing.py

--- LAB9/PIDfollowing.py
+++ LAB9/PIDfollowing.py
@@ -19,7 +19,7 @@ tcs.gain = 4                # valid gains: 1, 4, 16, 60
 # PID + control params
 # -----------------------
 Kp = 10
-Ki = 5
+Ki = 3
 Kd = 150
 
 # The original code used "PWM" in [0..255]. We'll compute in that domain,
