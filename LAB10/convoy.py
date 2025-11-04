bd5ef0c5c63331f579c30dd2bc822f27b79dce4e
ollisam <olivers23@ru.is>
2025-11-04 17:54:51 +0000

    Update convoy.py

--- LAB10/convoy.py
+++ LAB10/convoy.py
@@ -124,6 +124,8 @@ def check_obstacle_now():
     """
     global last_range_sample_t, last_voltage, blocked, clear_count
 
+    print(last_voltage)
+
     now = time.monotonic()
     if (now - last_range_sample_t) < SAMPLE_INTERVAL:
         return  # respect sampling rate
